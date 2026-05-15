284ccb83181aebc0c463aa0747cac7a7ba026936
LuigiPzz <luigipozzi.it@gmail.com>
2026-05-15 22:25:47 +0200

    Fix: add missing lazy items import in HomeScreen.kt

--- app/src/main/java/com/nothing/card/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/nothing/card/ui/screens/home/HomeScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.shape.CircleShape
